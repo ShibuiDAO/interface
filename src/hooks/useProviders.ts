61e7ba94210250f4be4491f32579c1b581cdb0ed
Nejc Drobnic <ethoarx@gmail.com>
2022-01-14 13:31:30 +0100

    fix: add default provider for rinkeby

--- src/hooks/useProviders.ts
+++ src/hooks/useProviders.ts
@@ -2,9 +2,11 @@ import { JsonRpcProvider, Provider } from '@ethersproject/providers';
 import { SupportedChainId } from 'constants/chains';
 
 const bobaProvider = new JsonRpcProvider('https://lightning-replica.boba.network', SupportedChainId.BOBA);
+const bobaRinkebyProvider = new JsonRpcProvider('https://rinkeby.boba.network/', SupportedChainId.BOBA_RINKEBY);
 
 export default function useProviders(): { [K in SupportedChainId]: Provider } {
 	return {
-		[SupportedChainId.BOBA]: bobaProvider
+		[SupportedChainId.BOBA]: bobaProvider,
+		[SupportedChainId.BOBA_RINKEBY]: bobaRinkebyProvider
 	};
 }
