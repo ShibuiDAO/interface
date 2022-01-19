5dd42d8c55e18ad72287a7305ae0ca551bdf2890
Nejc Drobnic <ethoarx@gmail.com>
2022-01-19 23:13:07 +0100

    fix: use correct popup colours

--- src/components/Assets/Order/Approve.tsx
+++ src/components/Assets/Order/Approve.tsx
@@ -69,8 +69,8 @@ const Approve: React.FC = () => {
 								leaveFrom="opacity-100 translate-y-0 sm:scale-100"
 								leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
 							>
-								<div className="z-40 inline-block rounded-md text-left overflow-hidden shadow-xl transform transition-all align-middle">
-									<div className="dark:text-white dark:bg-gray-900 border dark:border-white border-transparent p-4">
+								<div className="z-40 inline-block text-left overflow-hidden shadow-xl transform transition-all align-middle">
+									<div className="dark:text-white dark:bg-darks-400 border dark:border-lights-200 border-transparent rounded-2xl p-4">
 										<div className="flex items-start">
 											<div className="mt-0 text-left">
 												<Dialog.Title as="span" className="w-full text-lg leading-6 font-bold">
--- src/components/Connect/Connect.tsx
+++ src/components/Connect/Connect.tsx
@@ -50,8 +50,8 @@ const Connect: React.FC = () => {
 								leaveFrom="opacity-100 translate-y-0 sm:scale-100"
 								leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
 							>
-								<div className="z-50 inline-block rounded-md text-left overflow-hidden shadow-xl transform transition-all align-middle">
-									<div className="dark:text-white dark:bg-gray-900 border dark:border-gray-700 border-transparent p-4">
+								<div className="z-50 inline-block text-left overflow-hidden shadow-xl transform transition-all align-middle">
+									<div className="dark:text-white dark:bg-darks-400 border dark:border-lights-300 rounded-2xl border-transparent p-4">
 										<div className="flex items-start">
 											<div className="mt-0 text-left">
 												<Dialog.Title as="span" className="w-full text-lg leading-6 font-bold">
