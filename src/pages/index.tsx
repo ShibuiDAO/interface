cdc8f8efec8646f93486f299b530df1ab13949c2
Nejc Drobnič <ethoarx@gmail.com>
2022-01-19 06:39:17 +0100

    refactor: style background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,27 +7,34 @@ import React from 'react';
 const Index: NextPage = () => {
 	return (
 		<>
-			<div className="container">
-				<div className="hero min-h-screen">
-					<div className="flex-col hero-content lg:flex-col-reverse">
-						<div className="text-left">
-							<h1 className="mb-5 text-5xl font-bold capitalize">View, buy & sell BOBA NFTs</h1>
-							<p className="mb-5">
-								Shibui marketplace is a BOBA-only NFT marketplace with super low fees and governed by the community.
-							</p>
-							<div className="mb-5 flex gap-4">
-								<a href={TWITTER_LINK} target="_blank" rel="noopener noreferrer">
-									<FontAwesomeIcon icon={faTwitter} />
-								</a>
-								<a href={GITHUB_LINK} target="_blank" rel="noopener noreferrer">
-									<FontAwesomeIcon icon={faGithub} />
-								</a>
-								<a href="" target="_blank" rel="noopener noreferrer">
-									<FontAwesomeIcon icon={faDiscord} />
-								</a>
-								<a href="" target="_blank" rel="noopener noreferrer">
-									<FontAwesomeIcon icon={faTelegram} />
-								</a>
+			<div
+				style={{
+					backgroundImage: 'url(/assets/misc/background.svg)',
+					backgroundSize: 'cover'
+				}}
+			>
+				<div className="container">
+					<div className="hero min-h-screen">
+						<div className="flex-col hero-content lg:flex-col-reverse">
+							<div className="text-left">
+								<h1 className="mb-5 text-5xl font-bold capitalize">View, buy & sell BOBA NFTs</h1>
+								<p className="mb-5">
+									Shibui marketplace is a BOBA-only NFT marketplace with super low fees and governed by the community.
+								</p>
+								<div className="mb-5 flex gap-4">
+									<a href={TWITTER_LINK} target="_blank" rel="noopener noreferrer">
+										<FontAwesomeIcon icon={faTwitter} />
+									</a>
+									<a href={GITHUB_LINK} target="_blank" rel="noopener noreferrer">
+										<FontAwesomeIcon icon={faGithub} />
+									</a>
+									<a href="" target="_blank" rel="noopener noreferrer">
+										<FontAwesomeIcon icon={faDiscord} />
+									</a>
+									<a href="" target="_blank" rel="noopener noreferrer">
+										<FontAwesomeIcon icon={faTelegram} />
+									</a>
+								</div>
 							</div>
 						</div>
 					</div>
