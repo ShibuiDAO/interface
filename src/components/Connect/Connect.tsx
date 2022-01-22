63eb6b523c7cca13155cd320fc08ed44ed867576
Nejc Drobnic <ethoarx@gmail.com>
2022-01-22 22:53:59 +0100

    refactor: connect modal use generic

--- src/components/Connect/Connect.tsx
+++ src/components/Connect/Connect.tsx
@@ -1,9 +1,7 @@
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Dialog, Transition } from '@headlessui/react';
+import GenericModal from 'components/Modals/GenericModal';
 import { useActiveWeb3React } from 'hooks/useActiveWeb3React';
 import useEagerConnect from 'hooks/useEagerConnect';
-import React, { Fragment, useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectConnectingStatus, setConnectingStatus, setEagerAttempt } from 'state/reducers/user';
 import MetamaskConnect from './MetamaskConnect';
@@ -16,72 +14,21 @@ const Connect: React.FC = () => {
 	const triedToEagerConnect = useEagerConnect();
 	const open = useSelector(selectConnectingStatus);
 
-	const closeButtonRef = useRef(null);
-
 	useEffect(() => {
 		dispatch(setEagerAttempt(triedToEagerConnect));
 	}, [triedToEagerConnect, active, dispatch]);
 
 	return (
-		<>
-			<Transition.Root show={open} as={Fragment}>
-				<Dialog
-					as="div"
-					className="fixed min-h-screen z-40 inset-0 overflow-y-auto m-auto min-w-max"
-					onClose={(state) => dispatch(setConnectingStatus(state))}
-				>
-					<div className="flex min-h-screen w-full">
-						<div className="flex items-end justify-center m-auto text-center">
-							<Transition.Child
-								as={Fragment}
-								enter="ease-out duration-300"
-								enterFrom="opacity-0"
-								enterTo="opacity-100"
-								leave="ease-in duration-200"
-								leaveFrom="opacity-100"
-								leaveTo="opacity-0"
-							>
-								<Dialog.Overlay className="fixed inset-0" />
-							</Transition.Child>
-							<Transition.Child
-								as={Fragment}
-								enter="ease-out duration-300"
-								enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-								enterTo="opacity-100 translate-y-0 sm:scale-100"
-								leave="ease-in duration-200"
-								leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-								leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-							>
-								<div className="z-50 inline-block text-left overflow-hidden shadow-xl transform transition-all align-middle">
-									<div className="dark:text-white dark:bg-darks-400 border dark:border-lights-300 rounded-2xl border-transparent p-4">
-										<div className="flex items-start">
-											<div className="mt-0 text-left">
-												<Dialog.Title as="span" className="w-full text-lg leading-6 font-bold">
-													Connect a wallet
-													<button
-														type="button"
-														className="float-right"
-														onClick={() => dispatch(setConnectingStatus(false))}
-														ref={closeButtonRef}
-													>
-														<FontAwesomeIcon icon={faTimes} />
-													</button>
-												</Dialog.Title>
-												<div className="mt-2 px-8">
-													<MetamaskConnect />
-													<WalletConnectConnect />
-													<WalletLinkConnect />
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</Transition.Child>
-						</div>
-					</div>
-				</Dialog>
-			</Transition.Root>
-		</>
+		<GenericModal
+			show={open}
+			onDialogClose={(state) => dispatch(setConnectingStatus(state))}
+			modalTitle="Connect a wallet"
+			onTitleCloseClick={() => dispatch(setConnectingStatus(false))}
+		>
+			<MetamaskConnect />
+			<WalletConnectConnect />
+			<WalletLinkConnect />
+		</GenericModal>
 	);
 };
 
