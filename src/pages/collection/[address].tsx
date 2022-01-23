eb75e72529db0b2cf4126fca243509f3ef818129
Nejc Drobnic <ethoarx@gmail.com>
2022-01-23 01:31:35 +0100

    fix: handle invalid collections

--- src/pages/collection/[address].tsx
+++ src/pages/collection/[address].tsx
@@ -1,5 +1,11 @@
+import { useQuery } from '@apollo/client';
+import { EIP721Response } from 'client';
+import { ChainSubgraphSets, generateEIP721CollectionExistsQuery } from 'client/queries';
 import CollectionAssets from 'components/Collection/CollectionAssets';
 import Offset from 'components/Navbar/Offset';
+import { SupportedChainId } from 'constants/chains';
+import { DEFAULT_CHAIN } from 'constants/misc';
+import { useActiveWeb3React } from 'hooks/useActiveWeb3React';
 import useForceConnectMenu from 'hooks/useForceConnectMenu';
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
@@ -11,6 +17,25 @@ const CollectionDisplay: NextPage = () => {
 
 	const address = Array.isArray(router.query.address) ? router.query.address[0] : router.query.address;
 	const addressNormalised = address?.toLowerCase();
+
+	const { chainId } = useActiveWeb3React();
+	const chainIdNormalised: SupportedChainId = chainId || DEFAULT_CHAIN;
+
+	const [EIP721Subgraph] = ChainSubgraphSets[chainIdNormalised];
+
+	const { data: collectionData, loading: collectionLoading } = useQuery<EIP721Response<'erc721Contract'>>(
+		generateEIP721CollectionExistsQuery(chainIdNormalised),
+		{
+			variables: { contract: addressNormalised },
+			context: { subgraph: EIP721Subgraph },
+			fetchPolicy: 'no-cache'
+		}
+	);
+
+	if (!collectionLoading && collectionData?.erc721Contract === null && typeof window !== 'undefined') {
+		void router.push('/404');
+		return null;
+	}
 
 	return (
 		<>
