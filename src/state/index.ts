74126dff91690f47a942efd21c7d524177cbecab
Nejc Drobnic <ethoarx@gmail.com>
2022-01-28 02:56:25 +0100

    feat: store collection info state

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import assetsReducer from './reducers/assets';
+import collectionsReducer from './reducers/collections';
 import ordersReducer from './reducers/orders';
 import transactionsReducer from './reducers/transactions';
 import userReducer from './reducers/user';
@@ -7,6 +8,7 @@ import userReducer from './reducers/user';
 export const store = configureStore({
 	reducer: {
 		assets: assetsReducer,
+		collections: collectionsReducer,
 		orders: ordersReducer,
 		transactions: transactionsReducer,
 		user: userReducer
