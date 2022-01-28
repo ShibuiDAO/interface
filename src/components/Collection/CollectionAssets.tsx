40ac9e1c7f51a9d43e8f3295ff4105c022674d9e
Nejc Drobnic <ethoarx@gmail.com>
2022-01-28 08:26:08 +0100

    feat: sort assets by lowest price by lowest price by default

--- src/components/Collection/CollectionAssets.tsx
+++ src/components/Collection/CollectionAssets.tsx
@@ -1,15 +1,16 @@
 import { useQuery } from '@apollo/client';
-import type { Erc721Contract, Erc721Token } from '@subgraphs/eip721-matic';
 import { Account } from '@shibuidao/erc721exchange-types';
+import type { Erc721Contract, Erc721Token } from '@subgraphs/eip721-matic';
 import type { EIP721Response, ERC721ExchangeResponse } from 'client';
 import { ChainSubgraphSets, generateEIP721ContractQuery, generateERC721ExchangeQuery } from 'client/queries';
 import ERC721Asset from 'components/Assets/ERC721Asset';
 import { SupportedChainId } from 'constants/chains';
+import { COLLECTION_REFRESH_INTERVAL, DEFAULT_CHAIN } from 'constants/misc';
+import { BigNumber } from 'ethers';
 import { useActiveWeb3React } from 'hooks/useActiveWeb3React';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { fillBuyOrders, fillSellOrders } from 'state/reducers/orders';
-import { COLLECTION_REFRESH_INTERVAL, DEFAULT_CHAIN } from 'constants/misc';
 
 export interface CollectionAssetsProps {
 	address: string;
@@ -52,9 +53,24 @@ const CollectionAssets: React.FC<CollectionAssetsProps> = ({ address }) => {
 				<span>Loading</span>
 			) : (
 				<>
-					{(assetsData.erc721Contract as Erc721Contract).tokens.map((token: Erc721Token) => (
-						<ERC721Asset token={token} chainId={chainIdNormalised} key={token.id} />
-					))}
+					{((assetsData.erc721Contract as Erc721Contract).tokens as Erc721Token[])
+						.sort((a, b) => {
+							const { contractSellOrders } = exchangeData?.account as Account;
+							const orderA = contractSellOrders.find((order) => order.contract.id === a.contract.id && order.token === a.identifier);
+							const orderB = contractSellOrders.find((order) => order.contract.id === b.contract.id && order.token === b.identifier);
+
+							const aPricePresent = typeof orderA?.price !== 'undefined';
+							const bPricePresent = typeof orderB?.price !== 'undefined';
+
+							return (
+								Number(bPricePresent) - Number(aPricePresent) ||
+								(aPricePresent && BigNumber.from(orderA.price).sub(BigNumber.from(orderB?.price)).div(1000000000000).toNumber()) ||
+								0
+							);
+						})
+						.map((token) => (
+							<ERC721Asset token={token} chainId={chainIdNormalised} key={token.id} />
+						))}
 				</>
 			)}
 		</>
