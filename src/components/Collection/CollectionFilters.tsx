bb1b159af9e41cbae126f0328bcf1c9bd44e19ff
Nejc Drobnic <ethoarx@gmail.com>
2022-02-14 23:13:49 +0100

    feat: style search and sort bars

--- src/components/Collection/CollectionFilters.tsx
+++ src/components/Collection/CollectionFilters.tsx
@@ -26,25 +26,30 @@ const CollectionFilters: React.FC<CollectionFiltersProps> = ({ className }) => {
 	return (
 		<div className={className}>
 			<form>
-				<div>
-					<input
-						name="identifierSearch"
-						id="identifierSearch"
-						type="search"
-						value={collectionAssetsSearch}
-						onChange={(ev) => dispatch(setCollectionAssetsSearch(ev.target.value))}
-					/>
-				</div>
-				<div>
-					<select
-						name="sorting"
-						id="sorting"
-						value={priceSorting}
-						onChange={(ev) => dispatch(setPriceSorting(Number(ev.target.value) as PriceSorting))}
-					>
-						<option value={PriceSorting.LtH}>Price: Low to High</option>
-						<option value={PriceSorting.HtL}>Price: High to Low</option>
-					</select>
+				<div className="flex min-w-full">
+					<div className="w-3/4 px-4">
+						<input
+							name="identifierSearch"
+							id="identifierSearch"
+							type="search"
+							value={collectionAssetsSearch}
+							onChange={(ev) => dispatch(setCollectionAssetsSearch(ev.target.value))}
+							placeholder="Search"
+							className="w-full rounded-xl p-2 py-3 dark:bg-darks-100 dark:text-black"
+						/>
+					</div>
+					<div className="w-1/4 px-4">
+						<select
+							name="sorting"
+							id="sorting"
+							value={priceSorting}
+							onChange={(ev) => dispatch(setPriceSorting(Number(ev.target.value) as PriceSorting))}
+							className="w-full rounded-xl p-2 py-3 dark:bg-darks-100 dark:text-black"
+						>
+							<option value={PriceSorting.LtH}>Price: Low to High</option>
+							<option value={PriceSorting.HtL}>Price: High to Low</option>
+						</select>
+					</div>
 				</div>
 			</form>
 		</div>
