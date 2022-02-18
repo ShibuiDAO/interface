f132ebb6ea573f5715add1c42b9455cb473525f5
Nejc Drobnic <ethoarx@gmail.com>
2022-02-18 18:55:53 +0100

    refactor: expand meta tags

--- src/DefaultSeoProps.ts
+++ src/DefaultSeoProps.ts
@@ -1,9 +1,12 @@
 import type { DefaultSeoProps as DefaultSeoPropsType } from 'next-seo';
 
 export const BaseUrl = 'https://shibuinft.com';
+export const Summary = 'A fully on-chain NFT marketplace moving trading outside of the hands of centralised platforms.';
+
 export const DefaultSeoProps: DefaultSeoPropsType = {
 	titleTemplate: 'ShibuiNFT | %s',
 	title: 'Home',
+	description: Summary,
 	canonical: BaseUrl,
 	additionalMetaTags: [
 		{ name: 'url', content: BaseUrl },
@@ -12,10 +15,10 @@ export const DefaultSeoProps: DefaultSeoPropsType = {
 		{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
 		{ name: 'robots', content: 'archive,follow,imageindex,index,odp,snippet,translate' },
 		{ name: 'googlebot', content: 'index,follow' },
-		// { name: 'author', content: `ShibuiDAO, contact@shibuidao.com` },
-		// { name: 'owner', content: `ShibuiDAO, contact@shibuidao.com` },
-		// { name: 'designer', content: `ShibuiDAO, contact@shibuidao.com` },
-		// { name: 'reply-to', content: 'contact@shibuidao.com' },
+		{ name: 'author', content: `ShibuiDAO, contact@shibuidao.com` },
+		{ name: 'owner', content: `ShibuiDAO, contact@shibuidao.com` },
+		{ name: 'designer', content: `ShibuiDAO, contact@shibuidao.com` },
+		{ name: 'reply-to', content: 'contact@shibuidao.com' },
 		{ name: 'target', content: 'all' },
 		{ name: 'audience', content: 'all' },
 		{ name: 'coverage', content: 'Worldwide' },
@@ -26,12 +29,13 @@ export const DefaultSeoProps: DefaultSeoPropsType = {
 		{ name: 'HandheldFriendly', content: 'True' },
 		{ name: 'apple-mobile-web-app-title', content: 'ShibuiNFT Marketplace' },
 		{ name: 'application-name', content: 'ShibuiNFT' },
-		{ name: 'revisit-after', content: '7 days' }
-		// { property: 'og:email', content: 'contact@shibuidao.com' }
+		{ name: 'revisit-after', content: '7 days' },
+		{ property: 'og:email', content: 'contact@shibuidao.com' }
 	],
 	openGraph: {
 		title: 'ShibuiNFT Marketplace',
 		url: BaseUrl,
+		description: Summary,
 		type: 'website',
 		locale: 'en_US',
 		site_name: 'ShibuiNFT',
