24de96419159b3eb52c6acd3bb4d88f5d0ee2db2
Nejc Drobnic <ethoarx@gmail.com>
2022-03-07 20:30:37 +0100

    fix: correct data for testnet deploy

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -1,7 +1,7 @@
 import { SupportedChainId } from './chains';
 
 const BOBA_MAINNET_ERC721_EXCHANGE = '0x02af48a420f0934ecfc1c34f6da83db1e3e56af7';
-const BOBA_RINKEBY_ERC721_EXCHANGE = '0x76352a916b57800732d5f16d9b511c506ded850c';
+const BOBA_RINKEBY_ERC721_EXCHANGE = '0x34755A949E68b18F585eB91711351b697C1563d5';
 
 export const ERC721_EXCHANGE: { [K in SupportedChainId]: `0x${string}` } = {
 	[SupportedChainId.BOBA]: BOBA_MAINNET_ERC721_EXCHANGE,
