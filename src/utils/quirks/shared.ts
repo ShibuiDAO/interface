18dc4c671d3902661aaa55ac1ef9936fca58a7dc
Nejc Drobnic <ethoarx@gmail.com>
2022-03-14 20:38:22 +0100

    fix: correctly render svg

--- src/utils/quirks/shared.ts
+++ src/utils/quirks/shared.ts
@@ -1,8 +1,9 @@
-export function quirkSVGImage(uri: string, uriStructure: URL): [uri: string] {
-	if (uriStructure.protocol !== 'data:') return [uri];
-	const svgPart = uri.split(',')[1];
-	if (!svgPart.includes('svg')) return [uri];
-	const blob = new Blob([svgPart], { type: 'image/svg+xml' });
+export function quirkSVGImage(uri: string, _: URL): [uri: string] {
+	// if (uriStructure.protocol !== 'data:') return [uri];
+	// const svgPart = uri.split(',')[1];
+	// if (!svgPart.includes('svg')) return [uri];
+	// const blob = new Blob([svgPart], { type: 'image/svg+xml' });
 
-	return [URL.createObjectURL(blob)];
+	// return [URL.createObjectURL(blob)];
+	return [uri];
 }
