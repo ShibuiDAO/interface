311b270bf123f2e1e5d102a51611700418941f22
Nejc Drobnic <ethoarx@gmail.com>
2022-03-29 18:35:34 +0200

    refactor: expand bridge form

--- src/pages/app/bridge.tsx
+++ src/pages/app/bridge.tsx
@@ -1,14 +1,26 @@
 /* eslint-disable no-alert */
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import FormFieldError from 'components/forms/FormFieldError';
 import FormFieldInfo from 'components/forms/FormFieldInfo';
+import { SupportedChainId } from 'constants/chains';
+import { DEFAULT_CHAIN } from 'constants/misc';
 import { Form, Formik } from 'formik';
+import { useActiveWeb3React } from 'hooks/useActiveWeb3React';
 import useForceConnectMenu from 'hooks/useForceConnectMenu';
+import useProviders from 'hooks/useProviders';
 import { NextPage } from 'next';
 import React from 'react';
+import { When } from 'react-if';
 import { BridgeFormSchema } from 'utils/schemas';
 
 const TestingMetadataPage: NextPage = () => {
 	useForceConnectMenu();
+	const { account, chainId, library } = useActiveWeb3React();
+
+	const chainIdNormalised: SupportedChainId = chainId || DEFAULT_CHAIN;
+	const fallbackProvider = useProviders()[chainIdNormalised];
+	const readonlyProvider = library || fallbackProvider;
 
 	return (
 		<>
@@ -19,76 +31,91 @@ const TestingMetadataPage: NextPage = () => {
 				}}
 			>
 				<div className="container">
-					<div className="flex min-h-screen w-full flex-row items-center justify-center">
+					<div className="flex min-h-[100vh] w-full flex-row items-center justify-center">
 						<div className="rounded-2xl border p-4 dark:border-lights-100 dark:bg-darks-400 dark:text-white">
 							<div className="p-4">
-								<>
-									<Formik
-										initialValues={{
-											l2Contract: '',
-											tokenId: '',
-											l1Receiver: ''
-										}}
+								<div className="rounded-xl border p-4 dark:border-lights-200 dark:bg-lights-300">
+									<div className="">
+										<div className="grid grid-cols-3 gap-8 text-center">
+											<div>Boba</div>
+											<div className="flex flex-row items-center justify-center">
+												<FontAwesomeIcon icon={faArrowRightLong} />
+											</div>
+											<div>Ethereum</div>
+										</div>
+									</div>
+								</div>
+								<div className="pt-4">
+									<>
+										<Formik
+											initialValues={{
+												l2Contract: '',
+												tokenId: '',
+												l1Receiver: ''
+											}}
 											validationSchema={BridgeFormSchema(chainIdNormalised, readonlyProvider)}
-										onSubmit={(data) => alert(data)}
-									>
-										{(props) => (
-											<Form>
-												<>
-													<div>
-														<div className="py-2">
-															<label htmlFor="l2Contract" className="mb-2 text-lg font-bold text-white">
-																L2 NFT Contract Address
-															</label>
-															<FormFieldInfo message="Address of the collection on the Boba L2 network" />
-															<input
-																name="l2Contract"
-																type="text"
-																onChange={props.handleChange}
-																value={props.values.l2Contract}
-																className="border-element-secondary outline-none block w-full rounded-lg border-b border-dotted bg-darks-200 pl-1 text-white"
-															/>
-															<FormFieldError message={props.errors.l2Contract} />
+											onSubmit={(data) => alert(data)}
+										>
+											{(props) => (
+												<Form>
+													<>
+														<div>
+															<div className="py-2">
+																<label htmlFor="l2Contract" className="mb-2 text-lg font-bold text-white">
+																	L2 NFT Contract Address
+																</label>
+																<FormFieldInfo message="Address of the collection on the Boba L2 network" />
+																<input
+																	name="l2Contract"
+																	type="text"
+																	onChange={props.handleChange}
+																	value={props.values.l2Contract}
+																	className="border-element-secondary outline-none block w-full rounded-lg border-b border-dotted bg-darks-200 px-2 text-white"
+																/>
+																<FormFieldError message={props.errors.l2Contract} />
+															</div>
+															<div className="py-2">
+																<label htmlFor="tokenId" className="mb-2 text-lg font-bold text-white">
+																	Token ID
+																</label>
+																<FormFieldInfo message="ID of the asset to bridge" />
+																<input
+																	name="tokenId"
+																	type="text"
+																	onChange={props.handleChange}
+																	value={props.values.tokenId}
+																	className="border-element-secondary outline-none block w-full rounded-lg border-b border-dotted bg-darks-200 px-2 pl-1 text-white"
+																/>
+																<FormFieldError message={props.errors.tokenId} />
+															</div>
+															<div className="py-2">
+																<label htmlFor="l1Receiver" className="mb-2 text-lg font-bold text-white">
+																	L1 Receiver
+																</label>
+																<FormFieldInfo message="The address to which to bridge on L1" />
+																<input
+																	name="l1Receiver"
+																	type="text"
+																	onChange={props.handleChange}
+																	value={props.values.l1Receiver}
+																	className="border-element-secondary outline-none block w-full rounded-lg border-b border-dotted bg-darks-200 px-2 pl-1 text-white"
+																/>
+																<FormFieldError message={props.errors.l1Receiver} />
+															</div>
 														</div>
-														<div className="py-2">
-															<label htmlFor="tokenId" className="mb-2 text-lg font-bold text-white">
-																Token ID
-															</label>
-															<FormFieldInfo message="ID of the asset to bridge" />
-															<input
-																name="tokenId"
-																type="text"
-																onChange={props.handleChange}
-																value={props.values.tokenId}
-																className="border-element-secondary outline-none block w-full rounded-lg border-b border-dotted bg-darks-200 pl-1 text-white"
-															/>
-															<FormFieldError message={props.errors.tokenId} />
+														<div className="mt-4">
+															<When condition={Boolean(account)}>
+																<button type="submit" className="btn bg-gray-200 text-black hover:bg-gray-400">
+																	Bridge
+																</button>
+															</When>
 														</div>
-														<div className="py-2">
-															<label htmlFor="l1Receiver" className="mb-2 text-lg font-bold text-white">
-																L1 Receiver
-															</label>
-															<FormFieldInfo message="The address to which to bridge on L1" />
-															<input
-																name="l1Receiver"
-																type="text"
-																onChange={props.handleChange}
-																value={props.values.l1Receiver}
-																className="border-element-secondary outline-none block w-full rounded-lg border-b border-dotted bg-darks-200 pl-1 text-white"
-															/>
-															<FormFieldError message={props.errors.l1Receiver} />
-														</div>
-													</div>
-													<div>
-														<button type="submit" className="btn bg-gray-200 text-black hover:bg-gray-400">
-															Bridge
-														</button>
-													</div>
-												</>
-											</Form>
-										)}
-									</Formik>
-								</>
+													</>
+												</Form>
+											)}
+										</Formik>
+									</>
+								</div>
 							</div>
 						</div>
 					</div>
