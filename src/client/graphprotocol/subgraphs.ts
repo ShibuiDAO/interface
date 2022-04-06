a9748a61909b6daf81eb14467b11653cd406bda1
Nejc Drobnic <ethoarx@gmail.com>
2022-04-06 22:16:45 +0200

    refactor: switch to hosted services subgraph for boba mainnet

--- src/client/graphprotocol/subgraphs.ts
+++ src/client/graphprotocol/subgraphs.ts
@@ -1,6 +1,6 @@
 export enum Subgraph {
-	BOBA_MAINNET_EIP721 = 'https://graph.mainnet.boba.network:8000/subgraphs/name/shibui/eip721-subgraph-boba',
+	BOBA_MAINNET_EIP721 = 'https://api.thegraph.com/subgraphs/name/quantumlyy/eip721-subgraph-boba',
 	BOBA_RINKEBY_EIP721 = 'https://graph.rinkeby.boba.network:8000/subgraphs/name/shibui/eip721-subgraph-boba',
-	BOBA_MAINNET_ERC721EXCHANGE = 'https://graph.mainnet.boba.network:8000/subgraphs/name/shibui/nft-exchange',
+	BOBA_MAINNET_ERC721EXCHANGE = 'https://api.thegraph.com/subgraphs/name/shibuidao/nft-exchange-boba-mainnet',
 	BOBA_RINKEBY_ERC721EXCHANGE = 'https://graph.rinkeby.boba.network:8000/subgraphs/name/shibui/nft-exchange'
 }
