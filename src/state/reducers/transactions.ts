d1455342f6d29cb3cb4f661e2e38d79fd81146f2
Nejc Drobnic <ethoarx@gmail.com>
2022-04-06 22:40:18 +0200

    refactor: log tx error to browser console

--- src/state/reducers/transactions.ts
+++ src/state/reducers/transactions.ts
@@ -42,7 +42,7 @@ export const transactionsSlice = createSlice({
 			.addMatcher(isAllOf(isTransactionAction, isAnyOf(isRejected, isRejectedWithValue)), (state, action) => {
 				state.pending = state.pending.filter((tx) => tx !== action.meta.requestId);
 				toast.error('Transaction failed.');
-				console.error(action.payload);
+				console.error('Transaction failed.', '\n', action.payload);
 			})
 			.addMatcher(isAllOf(isTransactionAction, isAnyOf(isFulfilled)), (state, action) => {
 				state.pending = state.pending.filter((tx) => tx !== action.meta.requestId);
