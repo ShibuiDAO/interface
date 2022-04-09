042f006395855ae1db4e330584368309d0b5bc09
Nejc Drobnic <ethoarx@gmail.com>
2022-04-09 08:07:46 +0200

    chore: add hedgey logo and update exclusions

--- src/constants/customLogos.ts
+++ src/constants/customLogos.ts
@@ -3,7 +3,8 @@ import { SupportedChainId } from './chains';
 export const customLogos: { [CID in SupportedChainId]: { [K: string]: string } } = {
 	28: {},
 	288: {
-		'0xce458fc7cfc322cdd65ec77cf7b6410002e2d793': 'turingMonsters.jpeg'
+		'0xce458fc7cfc322cdd65ec77cf7b6410002e2d793': 'turingMonsters.jpeg',
+		'0x2aa5d15eb36e5960d056e8fea6e7bb3e2a06a351': 'hedgeys.jpeg'
 	}
 };
 
--- src/constants/exclusions.ts
+++ src/constants/exclusions.ts
@@ -2,7 +2,7 @@ import { SupportedChainId } from './chains';
 
 export const exlusions: { [CID in SupportedChainId]: string[] } = {
 	28: [],
-	288: ['0x2b503dd5b4a6fc491a1f9eb1e7b67b679b9d95ba']
+	288: ['0x2b503dd5b4a6fc491a1f9eb1e7b67b679b9d95ba', '0x0b745239764097cdde9691d91a81745ce8022036', '0x317c5ea002dee83704207a022c942cdba29edb4b']
 };
 
 export default exlusions;
